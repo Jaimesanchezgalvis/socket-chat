a7e857b7d3845a4ce292fd13c6d64bdf973fc325
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-01-18 21:45:42 +0100

    feat: chat fin v2

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,7 +4,7 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has("nombre") || !params.has("sala")) {
   window.location = "index.html";
-  throw new Error("El nombre y la sala es necesario");
+  throw new Error("El nombre y sala son necesarios");
 }
 
 var usuario = {
@@ -12,11 +12,12 @@ var usuario = {
   sala: params.get("sala"),
 };
 
-// on = escuchar
 socket.on("connect", function () {
   console.log("Conectado al servidor");
+
   socket.emit("entrarChat", usuario, function (resp) {
-    console.log("Usuarios conectados", resp);
+    // console.log('Usuarios conectados', resp);
+    renderizarUsuarios(resp);
   });
 });
 
@@ -26,25 +27,24 @@ socket.on("disconnect", function () {
 });
 
 // Enviar información
-// socket.emit(
-//   "crearMensaje",
-//   {
-//     usuario: "Fernando",
-//     mensaje: "Hola Mundo",
-//   },
-//   function (resp) {
-//     console.log("respuesta server: ", resp);
-//   }
-// );
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on("crearMensaje", function (mensaje) {
-  console.log("Servidor:", mensaje);
+  //   console.log("Servidor:", mensaje);
+  renderizarMensajes(mensaje, false);
+  scrollBottom();
 });
 
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on("listaPersona", function (personas) {
-  console.log(personas);
+  renderizarUsuarios(personas);
 });
 
 // Mensajes privados
